d3b2d3ee7626dc081d80b82cc3a5035d2fa44319
Mehdi Adham <tablighatroom@gmail.com>
2022-07-30 14:25:38 +0430

    Add new file

---
+++ modbus.c
@@ -0,0 +1,10 @@
+/**
+ * @file modbus.c
+ * @author Mehdi Adham (mehdi.adham@yahoo.com)
+ * @brief This library implements the Modbus protocol.
+ * @version 0.1
+ * @date 2022-07-30
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
---
+++ modbus.h
@@ -0,0 +1,24 @@
+/**
+ * @file modbus.h
+ * @author Mehdi Adham (mehdi.adham@yahoo.com)
+ * @brief This library implements the Modbus protocols.
+ * @version 0.1
+ * @date 2022-07-30
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef __MODBUS_H
+#define __MODBUS_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
